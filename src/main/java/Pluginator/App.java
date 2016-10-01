bd902170360784e034f439b6e2fa6aaec81123e8
madamek <maksym100@gmail.com>
2016-10-01 16:47:22 +0200

    Sprzatam modul Plugin z Pluginatora

--- src/main/java/Pluginator/App.java
+++ src/main/java/Pluginator/App.java
@@ -23,35 +23,35 @@ import java.util.stream.Collectors;
  */
 public class App {
 
-	public static void main(String[] args) throws ClassNotFoundException, MalformedURLException, IllegalAccessException, InstantiationException, InterruptedException {
-		ApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
+  public static void main(String[] args) throws Exception {
+    ApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
 
-		while (true) {
-			((ConfigurableApplicationContext) context).refresh();
-			File feeds = new File("src/main/resources/feeds");
-			File[] feedsFiles = feeds.listFiles(pathname -> {
-				return pathname.getName().endsWith(".jar");
-			});
+    while (true) {
+      ((ConfigurableApplicationContext) context).refresh();
+      File feeds = new File("src/main/resources/feeds");
+      File[] feedsFiles = feeds.listFiles(pathname -> pathname.getName().endsWith(".jar"));
 
-			List<URL> urls = new ArrayList<>();
-			for (File feedsFile : feedsFiles) {
-				urls.add(feedsFile.toURL());
-			}
-			URLClassLoader child = new URLClassLoader(urls.toArray(new URL[0]), App.class.getClassLoader());
-			Class classToLoad = Class.forName("Plugin.CustomFeeder", true, child);
-			Object instance = classToLoad.newInstance();
-			Feeder feeder = (Feeder) instance;
-			ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) context).getBeanFactory();
-			beanFactory.registerSingleton(feeder.getClass().getCanonicalName(), feeder);
+      for (File feedsFile : feedsFiles) {
+        try {
+          URLClassLoader child =
+              new URLClassLoader(new URL[]{feedsFile.toURL()}, App.class.getClassLoader());
+          String feedsFileName = feedsFile.getName();
+          String feedName = feedsFileName.split("\\.")[0];
+          Class classToLoad = Class.forName(feedName, true, child);
+          Object instance = classToLoad.newInstance();
+          Feeder feeder = (Feeder) instance;
+          ConfigurableListableBeanFactory
+              beanFactory =
+              ((ConfigurableApplicationContext) context).getBeanFactory();
+          beanFactory.registerSingleton(feeder.getClass().getCanonicalName(), feeder);
 
-			context.getBeansOfType(Feeder.class).values().forEach(Feeder::sayHi);
-			Thread.sleep(2000);
-		}
-	}
+          context.getBeansOfType(Feeder.class).values().forEach(Feeder::sayHi);
+        } catch (Exception e) {
+          System.err.println("Wrong feedFile: " + feedsFile);
+        }
+      }
 
-	@Test
-	public void test() {
-		File file = new File("src/main/resources/feeds/com.plugin-1.0-SNAPSHOT.jar");
-		assert file.exists();
-	}
+      Thread.sleep(2000);
+    }
+  }
 }
